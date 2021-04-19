f8b12c0cea08fc557c0b74a60a96da389184e8f6
Gorostiola <79371160+Gorostiola@users.noreply.github.com>
2021-04-18 20:27:51 -0500

    Add files via upload

---
+++ Today.java
@@ -0,0 +1,75 @@
+
+package today;
+import java.util.Scanner;
+public class Today {
+    //    >  <
+
+    
+    public static void main(String[] args) {
+
+//        String nom="Gorostiola";
+//        System.out.println(nom.charAt(0)); // Metodo charAt()
+////        Arrays = length           String = length()
+//        
+        
+//        String n="jesus".toUpperCase(); //Mayusculas
+//        String n2="Gorostiola".toLowerCase(); // Minusculas
+//        System.out.println();
+        
+        
+//        String n3= "Hola mundo";  //Si la cadena no tiene el caracter, retornara un valor Negativo
+//        int i=n3.indexOf('H');      // Retornara la 1ra posicion 
+//        int i2=n3.lastIndexOf('d'); //  Retornara la ultima posicion 
+//        System.out.println(i2);
+        
+//        String nom= "Un buen libro de buen java";
+//        int p1= nom.indexOf("buen");
+//        int p2= nom.lastIndexOf("java");
+//        System.out.println(p1);
+//        System.out.println(p2);
+        
+        
+//        String nom= "Jesus Gorostiola Enriquez";
+//        String s=nom.substring(0,7); // metodo: .substring(7) o (0,7)
+//        System.out.println(s);
+        
+        
+//        String nom= "Tecnologico de estudios superiores";
+//        boolean b1= nom.startsWith("Tec"); //Verifica si comienza con un determinado prefijo
+//        boolean b2= nom.endsWith("riores");// Verifica si termina con un determinado prefijo
+//        System.out.println(b1); // True
+//        System.out.println(b2); // True
+        
+        
+//        int i= 43;
+//        String s= Integer.toString(i); //Convertir de Int a String
+//        int en=Integer.parseInt(s); // Convierto de String a Int
+//        
+//        double d= 2.2;
+//        String s1= Double.toString(d);//Convierto de Double a String
+//        double d1=Double.parseDouble(s1); // Convierto de String a Double
+        
+        //Primitivos: byte, short, char,      int,     long, float, double, boolean
+        //Wrapper:    Byte, Short, Character, Integer, Long, Float, Double, Boolean
+        
+        
+//        int n=5;  // Conversion de numeros
+//        System.out.println(Integer.toBinaryString(n));
+//        System.out.println(Integer.toOctalString(n));
+//        System.out.println(Integer.toHexString(n));
+//        
+        
+        PAGINA 54
+        
+        
+       
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
